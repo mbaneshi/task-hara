38b6620d017bc91beb51ccd4b4260dde2fef938d
Mehdi Baneshi <baneshi@gmail.com>
2022-07-04 03:48:51 +0430

    add task pic to public

---
+++ hara-task/src/components/MyApp.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Task from "./Task";
+
+export default function MyApp() {
+  return (
+    <React.Fragment>
+      <Task />
+    </React.Fragment>
+  );
+}
---
+++ hara-task/src/components/Task.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Task() {
+  return (
+    <React.Fragment>
+      <h1>Task</h1>
+      <p>This Task recieve from hara company</p>
+      <img src="http://localhost:8080/images/task.png" alt="Task" />
+    </React.Fragment>
+  );
+}
